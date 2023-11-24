ad3808c43429255a3a222046055846eec6a1b072
anaspacheco <asp9060@nyu.edu>
2023-11-24 13:30:35 -0500

    machine learning files

--- machine-learning-client/setup.py
+++ machine-learning-client/setup.py
@@ -1,48 +1,67 @@
+import cv2
+import numpy as np
 import mediapipe as mp
-import time 
-import cv2 
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.9)
+mpDraw = mp.solutions.drawing_utils
 
-images = []
-results = []
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    if(result == None):
-        return
-    gesture = result.gestures
-    print(gesture)
+# Load the gesture recognizer model
+model = load_model('machine-learning-client/mp_hand_gesture')
 
+# Load class names
+f = open('machine-learning-client/mp_hand_gesture/gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
 
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path='machine-learning-client/gesture_recognizer.task'),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+# Initialize the webcam for Hand Gesture Recognition Python project
+cap = cv2.VideoCapture(0)
 
-with GestureRecognizer.create_from_options(options) as recognizer:
+while True:
+  # Read each frame from the webcam
+  _, frame = cap.read()
+  x , y, c = frame.shape
 
-    print('gesture recognizer initialized')
-    cap = cv2.VideoCapture(0)
+  # Flip the frame vertically
+  frame = cv2.flip(frame, 1)
 
-    while cap.isOpened():
-        # Create a loop to read the latest frame from the camera using VideoCapture.read()
-        ret, frame = cap.read()
+  framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  result = hands.process(framergb)
 
-        if not ret:
-            print("Failed to capture frame")
-            break
+  className = ''
 
-        # Convert the frame received from OpenCV to a MediaPipe’s Image object.
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+  # post process the result
+  if result.multi_hand_landmarks:
+    landmarks = []
+    for handslms in result.multi_hand_landmarks:
+        for lm in handslms.landmark:
+            # print(id, lm)
+            lmx = int(lm.x * x)
+            lmy = int(lm.y * y)
+            landmarks.append([lmx, lmy])
 
-        recognizer.recognize_async(mp_image, round(time.time() * 1000))
+        # Drawing landmarks on frames
+        mpDraw.draw_landmarks(frame, handslms, 
+        mpHands.HAND_CONNECTIONS)
+  # Show the final output
+    # Predict gesture in Hand Gesture Recognition project
+    prediction = model.predict([landmarks])
+    print(prediction)
+    classID = np.argmax(prediction)
+    className = classNames[classID]
 
-        # Display the frame
-        cv2.imshow('Hand Tracking', frame)
+  # show the prediction on the frame
+  cv2.putText(frame, className, (10, 50), cv2.FONT_ITALIC,
+               	1, (128, 0, 128), 2, cv2.LINE_AA)
 
-        # Break the loop if 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+  cv2.imshow("Output", frame)
+  if cv2.waitKey(1) == ord('q'):
+    		break
+
+# release the webcam and destroy all active windows
+cap.release()
+cv2.destroyAllWindows()
+
