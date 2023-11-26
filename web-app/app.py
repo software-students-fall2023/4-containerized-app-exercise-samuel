a2090a0c00458e67d7b7da5d12285b5030e2aac8
Samuel Shally <90644405+SamuelShally@users.noreply.github.com>
2023-11-26 20:52:28 +0000

    LINTING

--- web-app/app.py
+++ web-app/app.py
@@ -1,25 +1,10 @@
+import os
+import sys
 from flask import (
     Flask,
     render_template,
-    request,
-    redirect,
-    abort,
-    url_for,
-    make_response,
-    session,
 )
-import pymongo
-from pymongo import MongoClient
-import os
-import dotenv
-from dotenv import load_dotenv
-from datetime import datetime
-import sys
 sys.path.append('../')
-from database import db
-
-#print(sys.path)
-
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -29,26 +14,25 @@ if os.getenv("FLASK_ENV", "development") == "development":
 
 
 @app.route("/")
-def hello(): 
+def hello():
     return render_template("welcome.html")
 
 @app.route("/victory", methods=["GET"])
-def victory(): 
+def victory():
     return render_template("victory.html")
 
 @app.route("/thumbsUp", methods=["GET"])
-def thumbsUp(): 
+def thumbs_up():
     return render_template("thumbsUp.html")
 
 @app.route("/thumbsDown", methods=["GET"])
-def thumbsDown(): 
+def thumbs_down():
     return render_template("thumbsDown.html")
 
 @app.route("/stop", methods=["GET"])
-def stop(): 
+def stop():
     return render_template("stop.html")
 
 @app.route("/rock", methods=["GET"])
-def rock(): 
+def rock():
     return render_template("rock.html")
-
