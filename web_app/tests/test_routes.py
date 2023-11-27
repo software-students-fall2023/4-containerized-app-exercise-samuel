b799f70c8bb76add4cc7dd824d8a665f90bd20e2
Samuel Shally <90644405+SamuelShally@users.noreply.github.com>
2023-11-27 01:57:24 +0000

    general update

--- web_app/tests/test_routes.py
+++ web_app/tests/test_routes.py
@@ -4,14 +4,16 @@ sys.path.append("..")
 
 from app import app
 
-# @pytest.fixture
-# def client():
-#     app.config['TESTING'] = True
-#     with app.test_client() as client:
-#         yield client
+# RUN WITH: python -m pytest
 
-# def test_hello_route(client):
-#     response = client.get("/")
-#     assert response.status_code == 200
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_route(client):
+    response = client.get("/")
+    assert response.status_code == 200
 
 # def 
