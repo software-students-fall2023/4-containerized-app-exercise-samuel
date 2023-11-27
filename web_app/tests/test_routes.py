380e08076ee06ef9d17259efb73c581a2e501657
Samuel Shally <90644405+SamuelShally@users.noreply.github.com>
2023-11-27 01:28:53 +0000

    Working on tests

---
+++ web_app/tests/test_routes.py
@@ -0,0 +1,18 @@
+import pytest
+import sys
+from flask import Flask
+sys.path.append("..")
+
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+# def test_hello_route(client):
+#     response = client.get("/")
+#     assert response.status_code == 200
+
+# def 
