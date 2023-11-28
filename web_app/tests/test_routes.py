2672a6314ec3fdf23d6743fa0b4aaf63e4588018
Lemon <qiwenruan11@gmail.com>
2023-11-28 13:57:52 -0500

    check if github is working again

--- web_app/tests/test_routes.py
+++ web_app/tests/test_routes.py
@@ -18,7 +18,7 @@ from app import app
 @pytest.fixture
 def client():
     """
-    Creates a flask testing client to simmulate calls to the web-app
+    Creates a flask testing client to simmulate calls to the web-app.
     """
     app.config["TESTING"] = True
     with app.test_client() as client:
