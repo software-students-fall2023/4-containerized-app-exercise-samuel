20ddbb4a6e9efa23d1343501840c9db909bd212e
Lemon <qiwenruan11@gmail.com>
2023-11-28 15:50:48 -0500

    test

--- web_app/tests/test_routes.py
+++ web_app/tests/test_routes.py
@@ -24,6 +24,7 @@ def client():
     Creates a flask testing client to simmulate calls to the web-app.
     """
     app.config["TESTING"] = True
+    mocker = Mock()
     with app.test_client() as client:
         yield client
 
