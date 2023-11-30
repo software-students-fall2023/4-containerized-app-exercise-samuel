fd5dc7ebc266ef97f0c16a2dd1382d3d6fcf6595
anaspacheco <asp9060@nyu.edu>
2023-11-29 19:21:59 -0500

    no tests

--- machine_learning_client/tests/test_ml.py
+++ machine_learning_client/tests/test_ml.py
@@ -1,16 +1,1 @@
-import cv2
-import numpy as np
-import pytest
-import certifi 
-import os
-import sys 
-from unittest.mock import Mock, patch
-
-sys.path.append("..")
-import machine_learning_client.setup 
-from machine_learning_client import setup
-
-
-
-
-
+ 
