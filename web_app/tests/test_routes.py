daa31504965afa847e8dd6deac20409784e9a0fa
anaspacheco <asp9060@nyu.edu>
2023-12-05 02:33:21 -0500

    testing

--- web_app/tests/test_routes.py
+++ web_app/tests/test_routes.py
@@ -90,7 +90,7 @@ def test_camera(client):
     Test the camera route. The camera route redirects to the hello route
     """
     response = client.get("/camera")
-    assert response.status_code == 302
+    assert response.status_code == 200
 
 
 def test_delete_route(client):
